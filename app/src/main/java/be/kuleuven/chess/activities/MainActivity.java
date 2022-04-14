567aa46781d1a8b873a5be5cf3d623e16df4d4d0
FatBoiiii <ivanbadenhorst10@gmail.com>
2022-04-14 15:12:42 +0200

    Folder structure

--- app/src/main/java/be/kuleuven/chess/activities/MainActivity.java
+++ app/src/main/java/be/kuleuven/chess/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package be.kuleuven.chess;
+package be.kuleuven.chess.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import be.kuleuven.chess.R;
 
 public class MainActivity extends AppCompatActivity {
 
