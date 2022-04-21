66bea213a712146e0a395bc16473b500751df961
FatBoiiii <ivanbadenhorst10@gmail.com>
2022-04-21 17:50:14 +0200

    Basic movement: King, Pawn, Rook

--- app/src/main/java/be/kuleuven/chess/models/Piece.java
+++ app/src/main/java/be/kuleuven/chess/models/Piece.java
@@ -35,4 +35,46 @@ public abstract class Piece {
         }
     }
 
+    public List<Tile> getMoves() {
+        return moves;
+    }
+
+    protected List<Tile> getStraightMoves(){
+        determineTile();
+
+        int[] pos = tile.getPosition();
+        List<Tile> moves = new ArrayList<>();
+        //first horizontal
+        for(int i = 0; i<8; i++){
+            if(i != pos[1]){
+                moves.add(board.getTile(pos[0], i));
+            }
+        }
+        //second vertical
+        for(int i = 0; i<8; i++){
+            if(i != pos[0]){
+                moves.add(board.getTile(i, pos[1]));
+            }
+        }
+        return moves;
+    }
+
+    protected List<Tile> getDiagonalMoves(){
+        determineTile();
+
+        int[] pos = tile.getPosition();
+        List<Tile> moves = new ArrayList<>();
+
+        for(int i = 0; i<8; i++){
+            int[] current = new int[2];
+            current[0] = pos[0] - i;
+            current[0] = pos[1] - i;
+            if(!(current[0] < 0 || current[1] < 0 )){
+                moves.add(board.getTile(current[0], current[1]));
+            }
+        }
+
+        return moves;
+
+    }
 }
--- app/src/main/java/be/kuleuven/chess/models/pieces/King.java
+++ app/src/main/java/be/kuleuven/chess/models/pieces/King.java
@@ -33,6 +33,58 @@ public class King extends Piece {
     @Override
     public void generateMoves()
     {
+        determineTile();
+        moves.clear();
+        int[] pos = tile.getPosition();
+        int[] bounds = getBound(pos[0], pos[1]);
+
+        for(int i = bounds[0]; i <= bounds[2]; i++)
+        {
+            for (int j = bounds[1]	; j<= bounds[3]; j++)
+            {
+                moves.add(board.getTile(i,j));
+            }
+        }
+
+
+
+    }
+
+    public int[] getBound(int row, int column){
+        //order: lowerH, lowerV, higherH, higherV
+        int[] ret = new int[4];
+
+
+        if(row == 0){
+            ret[0] = row;
+        }
+        else{
+            ret[0] = row-1;
+        }
+
+        if(column == 0){
+            ret[1] = column;
+        }
+        else{
+            ret[1] = column-1;
+        }
+
+        if(row == 7){
+            ret[2] = row;
+        }
+        else{
+            ret[2] = row + 1;
+        }
+
+        if(column == 7){
+            ret[3] = column;
+        }
+        else{
+            ret[3] = column + 1;
+        }
+
+        return ret;
+
 
     }
 }
